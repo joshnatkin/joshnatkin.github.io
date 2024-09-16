29cf5c86a639cfd15872b4ba62acf1ae78fe85d3
joshnatkin <jnatkin@email.sc.edu>
2024-09-15 22:59:52 -0400

    final

--- csce242/Assignments/Assignment5/script.js
+++ csce242/Assignments/Assignment5/script.js
@@ -17,10 +17,14 @@ imgRefresher.onclick = () => {
 }
 
 //slider
+const slider = document.getElementById('slider');
 const square = document.getElementById('square');
 
-slider.oninput = () => {
+slider.addEventListener('input', () => {
     const sliderValue = slider.value;
-    square.style.left = sliderValue + '%';
-};
-
+    const maxSliderValue = slider.max;
+    
+    const maxPosition = 100 - (square.offsetWidth / document.getElementById('slida').offsetWidth * 100);
+    const position = Math.min(sliderValue, maxSliderValue * maxPosition / 100);
+    square.style.left = position + '%';
+});
