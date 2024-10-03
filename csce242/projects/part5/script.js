40e01517f12141516b3a8de538809df141a4dfb0
joshnatkin <jnatkin@email.sc.edu>
2024-10-02 20:16:35 -0400

    f

--- csce242/projects/part5/script.js
+++ csce242/projects/part5/script.js
@@ -2,4 +2,4 @@ document.querySelector("#toggle-nav").addEventListener('click', (event) => {
     event.preventDefault();
     const navItems = document.querySelector("#nav-items");
     navItems.classList.toggle("show");
-});
+}); 
