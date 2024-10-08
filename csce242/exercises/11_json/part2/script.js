837740f26056f72a52aa131bd9309895a5309024
joshnatkin <jnatkin@email.sc.edu>
2024-10-08 13:09:10 -0400

    add everything

---
+++ csce242/exercises/11_json/part2/script.js
@@ -0,0 +1,28 @@
+const getBreweries = async() => {
+    try {
+        return (await fetch("https://api.openbrewerydb.org/breweries")).json();
+    } catch(error) {
+        console.log(error);
+    }
+}
+
+const showBreweries = async() => {
+    const breweries = await getBreweries();
+
+    breweries.forEach((brewery)=>{
+        const section = document.createElement("section");
+        section.classList.add("brewery");
+        document.getElementById("breweries-section").append(section);
+
+        //create the heading
+        const a = document.createElement("a");
+        a.href= brewery.website_url;
+        section.append(a);
+        const h3 = document.createElement("h3");
+        h3.innerHTML = brewery.name;
+        a.append(h3);
+        
+    });
+}
+
+showBreweries();
